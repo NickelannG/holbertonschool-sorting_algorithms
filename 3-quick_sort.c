4a19f99443dd90e39fffa87cbaf3a4e28e7a887a
NickelannG <8836@holbertonstudents.com>
2024-04-11 12:26:28 +0000

    Task 3: using quick sort in an array of integers

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,48 @@
 #include "sort.h"
 
 /**
+ * partition - partitions an array of integers
+ *
+ * @array: the array to be partitioned
+ * @high: the higher index of the partition
+ * @low: the lower index of the partition
+ * @size: the size of the array
+ */
+int partition(int *array, int high, int low, size_t size)
+{
+	int i, j, pivot, temp, temp2;
+
+	/* Choosing a pivot - last element */
+	pivot = array[high];
+
+	i = (low - 1);
+
+	j = low;
+	while (j <= high)
+	{
+		if (array[j] < pivot)
+		{
+			/* Swap */
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			
+			/* print the array after swap */
+			print_array(array, size);
+			i++;
+		}
+		j++;
+	}
+	/* Swap with pivot */
+	temp2 = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp2;
+
+	/* pivot index */
+	return (i + 1);
+}
+
+/**
  * quick_sort - sorts an array of integers in ascending order using
  * the quick sort algorithm
  *
@@ -10,4 +52,21 @@
  */
 void quick_sort(int *array, size_t size)
 {
+	size_t low = 0, high = size - 1;
+	int pindex; /* pivot index */
 
+	/* Base Case */
+	if (size < 2)
+		return;
+
+	while (low < high)
+	{
+		pindex = partition(array, low, high, size);
+
+		/* Sort lower sub-array */
+		quick_sort(array, pindex - low);
+		
+		/* Update for next iteration */
+		low = pindex + 1;
+	}
+}
