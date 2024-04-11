3f80a1fb3a2532657eb8ee39e84f4ddacff2f885
NickelannG <8836@holbertonstudents.com>
2024-04-11 10:35:45 +0000

    Task 3: set up

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using
+ * the quick sort algorithm
+ *
+ * @array: the array of integers to be sorted
+ * @size: the size of the array 
+ *
+ */
+void quick_sort(int *array, size_t size)
+{
+
