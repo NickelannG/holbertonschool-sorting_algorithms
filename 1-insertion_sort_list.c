ae644cafc9d327f66ffc7959798e0923f50d4ed6
NickelannG <8836@holbertonstudents.com>
2024-04-12 03:16:48 +0000

    Task 1 betty docs: removing whitespaces and fixing comment to be under 80 characters per line

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,38 +10,42 @@
 void insertion_sort_list(listint_t **list)
 
 {
-    /*declares three pointers to nodes of type listin_t*/
-    listint_t *current, *prev, *next;
+	/*declares three pointers to nodes of type listin_t*/
+	listint_t *current, *prev, *next;
 
-    /*Intial check if list is valid and not empty, a list with zero or one element is already considered sorted*/
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-    return;
+	/**
+	 * Intial check if list is valid and not empty,
+	 * a list with zero or one element is already considered sorted
+	 */
 
-    /*Looping through list starting from 2nd until reaches end of list */
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        next = current->next;
-        while (current->prev != NULL && current->n < current->prev->n)
-        {
-        prev = current->prev;
-        /*Swap nodes*/
-        if (prev->prev != NULL)
-        prev->prev->next = current;
-        else
-        *list = current;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-        if (current->next != NULL)
-        current->next->prev = prev;
+	/*Looping through list starting from 2nd until reaches end of list */
+	current = (*list)->next;
 
-        prev->next = current->next;
-        current->prev = prev->prev;
-        current->next = prev;
-        prev->prev = current;
+	while (current != NULL)
+	{
+		next = current->next;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			prev = current->prev;
 
-        /*Print list after swapping */
-        print_list(*list);
-        }
-         current = next;
-    }
+			/*Swap nodes*/
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			prev->next = current->next;
+			current->prev = prev->prev;
+			current->next = prev;
+			prev->prev = current;
+
+			/*Print list after swapping */
+			print_list(*list);
+		}
+		current = next;
+	}
 }
